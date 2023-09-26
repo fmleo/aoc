953fc6c953a3cb6db34a2e8c56fe748e18077353
leo <fmleonardo@protonmail.com>
2023-09-25 22:43:07 -0300

    fix(2019): :vomit:

--- 2019/01/1.py
+++ 2019/01/1.py
@@ -4,10 +4,6 @@ total = 0
 
 while True:
     try:
-        mass = int(input())
-        mass = floor(mass/3)
-        mass = mass - 2
-
         total += floor(int(input()) / 3) - 2
     except EOFError:
         print(total)
